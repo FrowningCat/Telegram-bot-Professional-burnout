779db222d40a65482c77c2dae9d55e08ef5d908b
FrowningCat <vsavinkov60@gmail.com>
2023-05-04 12:17:43 +0300

    The_second_brick_of_the_building

--- tg_bot_Burnout/main.py
+++ tg_bot_Burnout/main.py
@@ -1,15 +1,13 @@
 from aiogram import executor
 
-from token import dp
-import handlers
+from loader import dp
+from bot_commands import help, start, start_testing
 from bot_commands.type_bot_commands import set_default_commands
 
 
 async def on_startup(dispatcher):
-
-
-# Устанавливаем дефолтные команды
-await set_default_commands(dispatcher)
+    # Устанавливаем дефолтные команды
+    await set_default_commands(dispatcher)
 
 if __name__ == '__main__':
     executor.start_polling(dp, on_startup=on_startup)
